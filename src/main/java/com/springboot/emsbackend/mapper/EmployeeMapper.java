e5a0c38cb04e15a8570dccbce7edb0bdd9e5629f
Nitish-Joshi <nitishcjoshi@gmail.com>
2023-09-03 23:47:59 -0700

    Add EmployeeDto and EmployeeMapper

---
+++ src/main/java/com/springboot/emsbackend/mapper/EmployeeMapper.java
@@ -0,0 +1,26 @@
+package com.springboot.emsbackend.mapper;
+
+import com.springboot.emsbackend.dto.EmployeeDto;
+import com.springboot.emsbackend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee) {
+        return new EmployeeDto(
+            employee.getId(),
+            employee.getFirstName(),
+            employee.getLastName(),
+            employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto) {
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+
+}
